44c4cf1a2173d96feea9d057d915cb2ce5a59b30
William Siqueira <wsiqueir@redhat.com>
2019-07-15 13:25:48 -0300

    AF-2094: License headers

--- packages/microeditor-envelope/src/__tests__/GwtEditorWrapper.test.ts
+++ packages/microeditor-envelope/src/__tests__/GwtEditorWrapper.test.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { GwtEditor } from "../gwt/GwtEditor";
 import { GwtEditorWrapper } from "../gwt/GwtEditorWrapper";
 import { DummyEditor } from "./DummyEditor";
@@ -9,6 +25,7 @@ const MockEditor = jest.fn(() => ({
 }));
 
 const mockEditor = new MockEditor();
+
 const wrapper = new GwtEditorWrapper(mockEditor);
 
 describe("GwtEditorWrapper", () => {
@@ -37,7 +54,9 @@ describe("GwtEditorWrapper", () => {
   test("af_onOpen no element to remove", () => {
     let removedHeaderPanel = document.getElementById("workbenchHeaderPanel");
     expect(removedHeaderPanel).toBeFalsy();
+
     wrapper.af_onOpen();
+    
     removedHeaderPanel = document.getElementById("workbenchHeaderPanel");
     expect(removedHeaderPanel).toBeFalsy();
   });
