f92164e384c6f66b28d2cabd2870e963fef0473e
Tiago Bento <tiagobentofernandes@gmail.com>
2019-09-04 15:53:13 -0300

    Adding bpmn editor

---
+++ packages/unpacked-gwt-editors/bpmn/org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor/deferredjs/E37A7DC713F47ADB74C2EE804446E417/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.org_kie_workbench_common_stunner_kogito_KogitoBPMNEditor.runAsyncCallback2('UMC($hc)(2);\n//# sourceMappingURL=sourcemaps/E37A7DC713F47ADB74C2EE804446E417_sourceMap2.json \n//# sourceURL=org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor-2.js\n')
