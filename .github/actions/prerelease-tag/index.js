cbe2e276b5b361ae3023ac9abdd72c1fc0cb569a
Tiago Bento <tiagobentofernandes@gmail.com>
2019-10-28 13:09:07 -0300

    Simplify tag detection on prerelease workflow

--- .github/actions/prerelease-tag/index.js
+++
@@ -1,8 +0,0 @@
-const core = require("@actions/core");
-
-try {
-  const tag = core.getInput("ref").replace("-prerelease", "").split("/").pop();
-  core.setOutput("tag", tag);
-} catch (error) {
-  core.setFailed(error.message);
-}
