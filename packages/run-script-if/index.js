84c57eafaed88ac3eee49462a4c8a831cf67b987
Tiago Bento <tiagobentofernandes@gmail.com>
2021-05-26 22:01:10 -0300

    run-script-if accepting multiple commands

--- packages/run-script-if/index.js
+++ packages/run-script-if/index.js
@@ -20,56 +20,90 @@ const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const { spawn } = require("child_process");
 
-const argv = yargs(hideBin(process.argv)).options({
-  env: {
-    required: true,
-    alias: "e",
-    type: "string",
-    description: "Environment variable name",
-  },
-  eq: {
-    default: "true",
-    alias: "equals",
-    type: "string",
-    description: "Value to be compared with the environment variable.",
-  },
-  command: {
-    required: true,
-    alias: "c",
-    type: "string",
-    description: "Command to execute if environment variable has the desired value",
-  },
-  "run-if-empty": {
-    default: "true",
-    description: "Runs the command if the environment variable is not set.",
-  },
-}).argv;
+async function main() {
+  const argv = yargs(hideBin(process.argv)).options({
+    env: {
+      required: true,
+      alias: "e",
+      type: "string",
+      description: "Environment variable name",
+    },
+    eq: {
+      default: "true",
+      alias: "equals",
+      type: "string",
+      description: "Value to be compared with the environment variable.",
+    },
+    command: {
+      array: true,
+      required: true,
+      alias: "c",
+      type: "string",
+      description: "Command to execute if environment variable has the desired value",
+    },
+    "run-if-empty": {
+      default: "true",
+      description: "Runs the command if the environment variable is not set.",
+    },
+  }).argv;
 
-if (process.env[argv.env] && process.env[argv.env] !== argv.eq) {
-  console.info(`Skipping '${argv.c}' because environment variable '${argv.env}' is not equal to '${argv.eq}'.`);
-  process.exit(0);
+  const nCommands = argv.c.length;
+  const formattedCommandsToLog = `'${argv.c.join("', '")}'`;
+
+  if (process.env[argv.env] && process.env[argv.env] !== argv.eq) {
+    console.info(
+        `[run-script-if] Skipping ${nCommands} command(s): ${formattedCommandsToLog}. Environment variable '${argv.env}' is not equal to '${argv.eq}'.`
+    );
+    process.exit(0);
+  }
+
+  if (!process.env[argv.env] && argv["run-if-empty"] !== "true") {
+    console.info(
+        `[run-script-if] Skipping ${nCommands} command(s):${formattedCommandsToLog}. Environment variable '${argv.env}' is not set and --run-if-empty is "${argv["run-if-empty"]}".`
+    );
+    process.exit(0);
+  }
+
+  if (!process.env[argv.env]) {
+    console.info(
+        `[run-script-if] Running ${nCommands} command(s): ${formattedCommandsToLog}. Environment variable '${argv.env}' is not set and --run-if-empty is "${argv["run-if-empty"]}".`
+    );
+  } else {
+    console.info(
+        `[run-script-if] Running ${nCommands} commands(s): ${formattedCommandsToLog}. Environment variable '${argv.env}' is equal to '${argv.eq}'.`
+    );
+  }
+
+  let nCommandsFinished = 0;
+
+  for (const c of argv.c) {
+    await new Promise((res, rej) => {
+      let commandBin = c.split(" ")[0];
+      let commandArgs = c.split(" ").slice(1);
+
+      console.info(`[run-script-if] Running '${c}'`);
+
+      const command = spawn(commandBin, commandArgs, { stdio: "inherit" });
+      command.on("error", (data) => {
+        console.error(`[run-script-if] Error executing '${c}':`);
+        console.error(data.toString());
+        rej(1);
+      });
+
+      command.on("exit", (code) => {
+        if (code !== 0) {
+          rej(code);
+          return;
+        }
+
+        nCommandsFinished += 1;
+        console.info(`[run-script-if] Finished '${c}'`);
+        res();
+      });
+    });
+  }
 }
 
-if (!process.env[argv.env] && argv["run-if-empty"] !== "true") {
-  console.info(`Skipping '${argv.c}' because environment variable '${argv.env}' is not set.`);
-  process.exit(0);
-}
-
-if (!process.env[argv.env]) {
-  console.info(`Running '${argv.c}'. Environment variable '${argv.env}' is not set.`);
-} else {
-  console.info(`Running '${argv.c}'. Environment variable '${argv.env}' is equal to '${argv.eq}'.`);
-}
-
-let commandBin = argv.c.split(" ")[0];
-let commandArgs = argv.c.split(" ").slice(1);
-
-const command = spawn(commandBin, commandArgs, { stdio: "inherit" });
-command.on("error", (data) => {
-  console.error(`Error executing '${argv.c}':`);
-  console.error(data.toString());
-});
-
-command.on("exit", (code) => {
+main().catch((code) => {
   process.exit(code);
 });
